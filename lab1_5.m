92f6a7843a5a088bb600a78476fca8928d554fc9
Ingrid Sjöberg <Ingrid Sjberg>
2015-11-22 13:32:41 +0100

    gjort om, och kanske rätt

--- lab1_5.m
+++ lab1_5.m
@@ -7,44 +7,46 @@ clc
 
 %% Looking at data A
 
-nSampa = size(a,1); % number of samples
-ta = (0:nSampa-1)/fSampa; % time vector in seconds
+nSamp_a = size(a,1);            % number of samples
+t_a = (0:nSamp_a-1)/fSampa;     % time vector in seconds
 
-figure(1);clf();
-plot(ta,a)
+figure(1);
+plot(t_a,a)                     % plot signal
 xlabel('time in seconds')
-ylabel('recorded signal') % axis description is important!
+ylabel('recorded signal') 
 
-%% Blä
-asig = a(32001:1:48000);
+sig_a = a(32001:1:48000);       % Extraction of two best seconds
 
-aetfe = etfe(asig);
-bode(aetfe)
+%% Model estimation
 
-aar10 = ar(asig,10);
-bode(aar10)
-preda = predict(aar10, asig, 1);
-sound(preda, 8000)
+a_etfe = etfe(sig_a);           
+bode(a_etfe)
+hold on
+a_ar10 = ar(sig_a,10);
+bode(a_ar10)
+
 
 %% Loading O
-[o, fSampo] = audioread('O.wav'); % extract data and sampling frequency
-
-%% Looking at data O
-
-nSampo = size(o,1); % number of samples
-to = (0:nSampo-1)/fSampo; % time vector in seconds
-
-figure(1);clf();
-plot(to,o)
-xlabel('time in seconds')
-ylabel('recorded signal') % axis description is important!
+% [o, fSampo] = audioread('O.wav'); % extract data and sampling frequency
+% 
+% %% Looking at data O
+% 
+% nSampo = size(o,1); % number of samples
+% to = (0:nSampo-1)/fSampo; % time vector in seconds
+% 
+% figure(1);clf();
+% plot(to,o)
+% xlabel('time in seconds')
+% ylabel('recorded signal') % axis description is important!
 
 %% blaha
-osig = o(28000:1:44000);
-
-oetfe = etfe(osig);
-bode(oetfe)
-oar6 = ar(osig,6);
-predo = predict(oar6, osig, 1);
-sound(predo, 8000)
+% osig = o(28000:1:44000);
+% 
+% oetfe = etfe(osig);
+% bode(oetfe)
+% hold on
+% oar6 = ar(osig,6);
+% bode(oar6)
+% predo = predict(oar6, osig, 1);
+% sound(predo, 8000)
 
