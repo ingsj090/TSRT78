3eb78545685336afc734422d2549b3c88d9adf0d
Ingrid Sjöberg <Ingrid Sjberg>
2015-11-24 23:10:15 +0100

    KALRT!

--- lab1_6.m
+++ lab1_6.m
@@ -4,18 +4,24 @@ clc
 
 %% Speech encoding as GSM
 
+%Loading the sound file
 [y, fSamp] = audioread('timewarp.wav');
 
+% Declaration of sequence length and period time
 nSamp= size(y,1);
 t = (0:nSamp-1)/fSamp;
 
+% Plotting the signal in time domain
 figure(1);
-plot(t, y)                  % Plot of signal
+plot(t, y)
+axis tight
+title('Recorded signal')
 xlabel('time in seconds')
 ylabel('recorded signal')
 
-phrase = y(0.7*8000:3.3*8000-1);     %extraction of sentence
 
+% Extraction of the sentence (removal of excess samples)
+phrase = y(0.7*8000:3.3*8000-1);    
 N=length(phrase);
 
 %% Divide the signal into segments of 160 samples each
@@ -27,39 +33,41 @@ while(ind-1 <= length(phrase) - 160)
     i=i+1;
     ind=ind+160;
 end
-%% Estimate model AR(8) for each segment
+%% Simulation of the sound
 [rows, columns] = size(segments);
 
-phrase_2=[];
+phrase_2=[];    %array for the simulated signal
+phrase_3=[];    %Array for the simulated signal using A=1 for all 
 for i=1:rows
    % Preprocessing: detrend 
    detr=detrend(segments(i,:));
    
-   % model each segment with AR(8) 
+   % model each segment
    mod=ar(detr,18);             % Found AR(18) gave best result
    
    % Pulse train 
-   e=filter(mod.a,1,detr);
-   r=covf(e',100);
-   [A,D]=max(r(19:end));
-   ehat=zeros(columns,1);
-   ehat(1:D:end)=20*sqrt(A);   % times 20 gives better result
+   e=filter(mod.a,1,detr);     
+   r=covf(e',100);              % covariance of residuals
+   [A,D]=max(r(19:end));        % A=amplitude, D=period
+   ehat=zeros(columns,1);       % array for pulsetrain 
+   ehat(1:D:end)=20*sqrt(A);    % times 20 gives better result
    
    % Test amplitude = 1 for all segments
    ehat1=zeros(columns,1);
    ehat1(1:D:end)=1;
    
-   % Dealing with unstable poles
+   % Removing unstable poles
    b=fstab(mod.a,1/fSamp);
    
+   % Simulation of signal
    yhat=filter(1,b,ehat');
-   yhat1=filter(1,b,ehat1');
+   yhat1=filter(1,b,ehat1');    %for amplitude=1
    
-   
+   % Gathering all segments in one array 
    phrase_2 = [phrase_2 yhat];
-   phrase_3 = [phrase_2 yhat1];
+   phrase_3 = [phrase_2 yhat1]; %for amplitude=1
 end
-  
-sound(phrase_2)
-a=1
 
+% Play the simulated sound
+sound(phrase_2)
+sound(phrase_3)
